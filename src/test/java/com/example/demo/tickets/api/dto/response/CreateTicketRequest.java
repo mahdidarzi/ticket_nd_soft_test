9b91b9679292e93eb0de2f4580a192934f6f9f7b
mhubsources <mahdidarzi1024@gmail.com>
2026-08-15 11:51:40 +0330

    chore: CreateTicketRequest has been made + basic validation

---
+++ src/test/java/com/example/demo/tickets/api/dto/response/CreateTicketRequest.java
@@ -0,0 +1,16 @@
+package com.example.tickets.api.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateTicketRequest(
+
+        @NotBlank(message = "عنوان تیکت الزامی است")
+        @Size(max = 150, message = "عنوان تیکت نباید بیشتر از 150 کاراکتر باشد")
+        String title,
+
+        @NotBlank(message = "توضیحات تیکت الزامی است")
+        @Size(max = 2000, message = "توضیحات تیکت نباید بیشتر از 2000 کاراکتر باشد")
+        String description
+) {
+}
